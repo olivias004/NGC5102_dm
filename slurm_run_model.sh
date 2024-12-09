558f7fb914fa6e19bc2b8d0fac37179977a1e22d
Livi Silcock <livisilcock@gmail.com>
2024-12-09 11:26:56 +1100

    edited slurm file as well

--- slurm_run_model.sh
+++ slurm_run_model.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+#SBATCH --nodes=1                    # Number of nodes
+#SBATCH --ntasks-per-node=20         # Number of tasks per node
+#SBATCH --job-name=initial_chain     # Job name
+#SBATCH --mail-user=oliviarose004@protonmail.com  # Email for notifications
+#SBATCH --mail-type=ALL              # Get notifications for all events
+#SBATCH --mem-per-cpu=500MB          # Memory per CPU
+#SBATCH --time=10:00:00              # Wall time
 
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=20
-#SBATCH --job-name=initial_chain
-#SBATCH --mail-user=oliviarose004@protonmail.com
-#SBATCH --mail-type=ALL
-#SBATCH --mem-per-cpu=500
-#SBATCH --time=010:00:00
-#SBATCH --account=oz059
-
+# Purge existing modules to avoid conflicts
 module purge
-ml gcc/12.2.0
-ml openmpi/4.1.4
+
+# Load necessary modules
+module load gcc/12.2.0
+module load openmpi/4.1.4
+
+# Activate your Python environment
 source activate NGC5102
 
-mpirun -n 20  python JAM/model_a.py
+# Run the Python script with MPI
+mpirun -n 20 python JAM/model_a.py
 
