c977174ba32a3f895b45e35ffb10133b609ff3b4
Livi Silcock <livisilcock@gmail.com>
2024-12-21 13:15:26 +1100

    reverse?

--- JAM/model_a.py
+++ JAM/model_a.py
@@ -130,8 +130,8 @@ def run_mcmc(output_path, ndim=4, nwalkers=20, nsteps=5000):
 if __name__ == "__main__":
     # CONSTANTS
     ndim = 4
-    nwalkers = 1000
-    nsteps = 10000
+    nwalkers = 12
+    nsteps = 1000
 
     # Paths
     data_path = "/home/osilcock/DM_data/kwargs.pkl"
